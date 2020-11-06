338a7e138e49a57ab0b290a0e9e0bc82acc15504
Vendy Prum <vendy@thinkful.com>
2020-11-06 11:12:42 -0800

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+# Tic Tac Toe Game
+
+player1 = input("Please choose a marker: 'X' or 'O'")
+position = int(input('Please enter a number between 1-9:'))
+
+
+from IPython.display import clear_output
+
+def display_board(board):
+    # create the set up of the board as a 
